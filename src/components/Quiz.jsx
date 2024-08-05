ae54eb37ca85b4b5d696ec34fe289c81c14edc70
Harsh Seervi <harshservi48@gmail.com>
2024-08-05 23:00:59 +0530

    Added Some Question

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -13,6 +13,46 @@ const Quiz = () => {
       option: ["Narendra Modi", "Amit Shah", "Rahul Gandhi", "Imran Khan"],
       answer: "Imran Khan",
     },
+    {
+      Question: "Who is the President of the United States?",
+      option: ["Joe Biden", "Donald Trump", "Barack Obama", "George Bush"],
+      answer: "Joe Biden",
+    },
+    {
+      Question: "Who is the Chancellor of Germany?",
+      option: ["Angela Merkel", "Olaf Scholz", "Frank-Walter Steinmeier", "Gerhard Schröder"],
+      answer: "Olaf Scholz",
+    },
+    {
+      Question: "Who is the Prime Minister of the United Kingdom?",
+      option: ["Boris Johnson", "Theresa May", "David Cameron", "Rishi Sunak"],
+      answer: "Rishi Sunak",
+    },
+    {
+      Question: "Who is the President of France?",
+      option: ["Emmanuel Macron", "François Hollande", "Nicolas Sarkozy", "Jacques Chirac"],
+      answer: "Emmanuel Macron",
+    },
+    {
+      Question: "Who is the Prime Minister of Canada?",
+      option: ["Justin Trudeau", "Stephen Harper", "Jean Chrétien", "Paul Martin"],
+      answer: "Justin Trudeau",
+    },
+    {
+      Question: "Who is the Prime Minister of Australia?",
+      option: ["Scott Morrison", "Anthony Albanese", "Malcolm Turnbull", "Tony Abbott"],
+      answer: "Anthony Albanese",
+    },
+    {
+      Question: "Who is the President of Russia?",
+      option: ["Vladimir Putin", "Dmitry Medvedev", "Boris Yeltsin", "Mikhail Gorbachev"],
+      answer: "Vladimir Putin",
+    },
+    {
+      Question: "Who is the President of China?",
+      option: ["Xi Jinping", "Hu Jintao", "Jiang Zemin", "Deng Xiaoping"],
+      answer: "Xi Jinping",
+    }
   ];
 
   const [currentQuestion, setCurrentQuestion] = useState(0);
